2725c942a5ecfd5a4af7cbad269785c3dbec2884
JqyModi <1294432350@qq.com>
2018-04-08 19:33:43 +0800

    Initial Commit

---
+++ QQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ
+//
+//  Created by Mac on 2018/4/8.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ
+//
+//  Created by Mac on 2018/4/8.
+//  Copyright © 2018年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
